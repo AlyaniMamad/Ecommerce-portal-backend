c9bb6d87ddf06b36cb55db66cb7be0124a97c4f6
akshay0077 <kherakshay007@gmail.com>
2023-12-24 16:20:05 +0530

    private-routing with spinner

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import About from './pages/About'
 import Contact from './pages/Contact'
@@ -7,6 +7,8 @@ import Policy from './pages/Policy'
 import Pagenotfound from './pages/Pagenotfound'
 import Register from './pages/Auth/Register';
 import Login from './pages/Auth/Login';
+import Dashboard from './pages/user/Dashboard';
+import PrivateRoute from './components/Routes/PrivateRoute';
 
 function App() {
   return (
@@ -15,15 +17,17 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
-        
+        <Route path='/dashboard' element={<PrivateRoute />} >
+          <Route path='' element={<Dashboard />} />
+        </Route>
         <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/policy' element={<Policy/>} />
-        <Route path='/*' element={<Pagenotfound/>} />
-        
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/policy' element={<Policy />} />
+        <Route path='/*' element={<Pagenotfound />} />
+
 
       </Routes>
-      
+
     </>
   );
 }
