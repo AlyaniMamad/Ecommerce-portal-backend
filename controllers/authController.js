355f93f7052a1836df16fbed936292ba41d33250
akshay0077 <kherakshay007@gmail.com>
2023-12-24 09:13:01 +0530

    login api and basic setup of frontend

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
-import { hashPassword } from '../helpers/authHelper.js';
+import { comparePassword, hashPassword } from '../helpers/authHelper.js';
 import userModel from '../models/userModel.js'
+import JWT from 'jsonwebtoken' 
 
 export const registerController = async (req, res) => {
     try {
@@ -52,4 +53,73 @@ export const registerController = async (req, res) => {
     }
 };
 
-// export default { registerController };
+// Method- Post for Login 
+export const loginController = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        
+        //validation 
+        if (!email || !password) {
+            return res.status(404).send({
+                success: false,
+                message: 'invalid email or password'
+            })
+        }
+        
+        //check user
+        const user = await userModel.findOne({ email })
+        
+        if (!user) {
+            return res.status(404).send({
+                success: false,
+                message: 'user not found please make a account'
+            })
+        }
+
+        const match = await comparePassword(password, user.password)
+        
+        if (!match) {
+            return res.status(200).send({
+                success: false,
+                message: 'invalid password'
+                
+            })
+        }
+
+        //create a token
+
+        const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+        res.status(200).send({
+            success: true,
+            message: 'login successfully',
+            user: {
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address
+            },
+            token,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: 'Error in Login Time',
+            error
+        });
+    }
+};
+
+//test controller
+
+export const testController = (req, res) => {
+    try {
+        res.send("Process Routes");
+    } catch (error) {
+        console.log(error);
+        res.send({ error });
+    }
+};
+
+
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,11 +1,18 @@
 import express from 'express'
-import {registerController} from '../controllers/authController.js'
+import {registerController,loginController,testController} from '../controllers/authController.js'
+import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
 
 //create a seprate router object
 const router = express.Router();
 
 //routing listed
 //Register Router and Method is POST
-router.post('/register',registerController)
+router.post("/register", registerController)
+
+//Login Router and Method is POST
+router.post("/login",loginController)
+
+//testing perpose router
+router.get("/test",requireSignIn,isAdmin,testController)
 
 export default router;
