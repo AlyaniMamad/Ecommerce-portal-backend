1bf687c2e8c9164116b345732b70e23af925db69
akshay0077 <kherakshay007@gmail.com>
2023-12-24 15:07:42 +0530

    login and register with DB

--- front-end/src/pages/Auth/Login.js
+++ front-end/src/pages/Auth/Login.js
@@ -4,10 +4,12 @@ import axios from "axios";
 import { useNavigate, useLocation } from "react-router-dom";
 import toast from "react-hot-toast";
 import "../../styles/AuthStyles.css";
+import { useAuth } from "../../context/auth";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [auth, setAuth] = useAuth();
 
   const navigate = useNavigate();
   const location = useLocation();
@@ -16,12 +18,17 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/login", {
+      const res = await axios.post(`${process.env.REACT_APP_API}/api/v1/auth/login`, {
         email,
         password,
       });
       if (res && res.data.success) {
         toast.success(res.data && res.data.message);
+        setAuth({
+          ...auth,
+          user: res.data.user,
+          token: res.data.token,
+        });
         localStorage.setItem("auth", JSON.stringify(res.data));
         navigate(location.state || "/");
       } else {
